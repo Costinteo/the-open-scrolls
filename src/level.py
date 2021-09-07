0afebfdac826387ae93fc5f2c491542d228213d1
Costinteo <costinteo@gmail.com>
2021-09-08 00:57:42 +0300

    added first batch of sprites!

--- src/level.py
+++ src/level.py
@@ -24,6 +24,22 @@ class Level:
         self.enemies = dict()
         self.entities = dict()
 
+        self.parseLevelData(mapInfo, charData)
+
+
+    def draw(self):
+        for ent in self.entities.values():
+            # skip player and enemies so they get drawn on top of everything else
+            if ent.id == self.player.id or ent.id in self.enemies:
+                continue
+            ent.draw()
+
+        for enemy in self.enemies.values():
+            enemy.draw()
+        # draw player on top of everything else
+        self.entities[self.player.id].draw()
+
+    def parseLevelData(self, mapInfo, charData):
         # reading and parsing level data
         for y in range(self.height):
             line = mapInfo.readline()
@@ -39,19 +55,19 @@ class Level:
                 newEntity = None
 
                 if tile == "-":
-                    newEntity = Entity(self.screen, x=x, y=y, name="horizontal_wall", solid=True, sprite=f"sprites/{self.tileset}/horizontal_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="horizontal_wall", solid=True, sprite=f"sprites/{self.tileset}/horizontal_wall.png")
                 elif tile == "|":
-                    newEntity = Entity(self.screen, x=x, y=y, name="vertical_wall", solid=True, sprite=f"sprites/{self.tileset}/vertical_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="vertical_wall", solid=True, sprite=f"sprites/{self.tileset}/vertical_wall.png")
                 elif tile == "/":
-                    newEntity = Entity(self.screen, x=x, y=y, name="topleft_wall", solid=True, sprite=f"sprites/{self.tileset}/topleft_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="topleft_wall", solid=True, sprite=f"sprites/{self.tileset}/topleft_wall.png")
                 elif tile == "\\":
-                    newEntity = Entity(self.screen, x=x, y=y, name="topright_wall", solid=True, sprite=f"sprites/{self.tileset}/topright_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="topright_wall", solid=True, sprite=f"sprites/{self.tileset}/topright_wall.png")
                 elif tile == "L":
-                    newEntity = Entity(self.screen, x=x, y=y, name="botleft_wall", solid=True, sprite=f"sprites/{self.tileset}/botleft_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="botleft_wall", solid=True, sprite=f"sprites/{self.tileset}/botleft_wall.png")
                 elif tile == "J":
-                    newEntity = Entity(self.screen, x=x, y=y, name="botright_wall", solid=True, sprite=f"sprites/{self.tileset}/botright_wall")
+                    newEntity = Entity(self.screen, x=x, y=y, name="botright_wall", solid=True, sprite=f"sprites/{self.tileset}/botright_wall.png")
                 elif tile == ".":
-                    newEntity = Entity(self.screen, x=x, y=y, name="walkable", solid=False, sprite=f"sprites/{self.tileset}/walkable")
+                    newEntity = Entity(self.screen, x=x, y=y, name="floor", solid=False, sprite=f"sprites/{self.tileset}/floor.png")
                 elif tile == "P":
                     # only create the player if no current character is passed
                     # in constructor of level
@@ -62,7 +78,7 @@ class Level:
                         # it needs a special type of save structure that I haven't implemented yet
                         # TODO: implement save file that contains all player character data
                         inventory = src.util.DataParser.readInventoryData("[E]scimitar [E]iron_helmet")
-                        self.player = Character(self.screen, x=x, y=y, name=name, isPlayer=True, level=lvl, exp=exp, HP=hp, STA=st, MGK=mg, STR=strg, INT=intl, AGI=agi, LCK=lck, inventory=inventory)
+                        self.player = Character(self.screen, x=x, y=y, name=name, isPlayer=True, level=lvl, exp=exp, HP=hp, STA=st, MGK=mg, STR=strg, INT=intl, AGI=agi, LCK=lck, inventory=inventory, sprite=f"sprites/Player.png")
 
                     newEntity = self.player
                 elif tile == "x":
@@ -71,7 +87,7 @@ class Level:
                     charName = src.util.trimmedline(charData.readline())
                     name, lvl, exp, hp, st, mg, strg, intl, agi, lck = src.util.DataParser.readCharData(charName)
                     inventory = src.util.DataParser.readInventoryData(charData.readline())
-                    newEntity = Character(self.screen, x=x, y=y, name=name, level=lvl, exp=exp, HP=hp, STA=st, MGK=mg, STR=strg, INT=intl, AGI=agi, LCK=lck, inventory=inventory)
+                    newEntity = Character(self.screen, x=x, y=y, name=name, level=lvl, exp=exp, HP=hp, STA=st, MGK=mg, STR=strg, INT=intl, AGI=agi, LCK=lck, inventory=inventory, sprite=f"sprites/{charName}.png")
                     self.enemies[newEntity.id] = newEntity
 
                 # add entity to entity dict regardless of class
@@ -82,17 +98,7 @@ class Level:
                         self.matrix[y].append(newEntity)
                     else:
                         # if the entity we store is not a wall we have to store a walkable for it
-                        self.matrix[y].append(Entity(self.screen, x=x, y=y, name="walkable", solid=False, sprite=f"sprites/{self.tileset}/walkable"))
+                        floorBelowEntity = Entity(self.screen, x=x, y=y, name="floor", solid=False, sprite=f"sprites/{self.tileset}/floor.png")
+                        self.matrix[y].append(floorBelowEntity)
+                        self.entities[floorBelowEntity.id] = floorBelowEntity
         print() # prints newline
-
-    def draw(self):
-        for ent in self.entities.values():
-            # skip player and enemies so they get drawn on top of everything else
-            if ent.id == self.player.id or ent.id in self.enemies:
-                continue
-            ent.draw()
-
-        for enemy in self.enemies.values():
-            enemy.draw()
-        # draw player on top of everything else
-        self.entities[self.player.id].draw()
